c409936cf8643eabf79125580bb3d5de129db77e
Sriman-N <sriman411@gmail.com>
2023-06-27 16:38:36 -0400

    initial

---
+++ com/sriman/javaoop/patterns/CompositePattern/Employee.java
@@ -0,0 +1,14 @@
+package com.sriman.javaoop.patterns.CompositePattern;
+
+/**
+ * @author srima
+ */
+public interface Employee {
+    public int getId();
+    public String getName();
+    public double getSalary();
+    public void print();
+    public void add(Employee employee);
+    public void remove(Employee employee);
+    public Employee getChild(int i);
+}
